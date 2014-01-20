81fd69ae1d63b279edb745fba790b505edce79c9
Hideshi Ogoshi <hideshi.ogoshi@gmail.com>
2014-01-21 01:53:45 +0900

    Added template file

---
+++ template.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from pyawk import PyAwk
+class Template(PyAwk):
+    def begin(self):
+        pass
+
+    def action(self, S):
+        pass
+
+    def end(self):
+        pass
+
+if __name__ == '__main__':
+    Template().run()
