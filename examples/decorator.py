5f5fdc901d47a41b03779d4b2d9e84585dba31d8
Hideshi Ogoshi <hideshi.ogoshi@gmail.com>
2014-01-23 17:41:45 +0900

    Added decorator.py

---
+++ examples/decorator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3.3
+from pyawk import PyAwk
+def decorate_action(method):
+    def wrapper(self, *args):
+        self.print('decorate_action start')
+        method(self, *args)
+        self.print('decorate_action finish')
+        self.print()
+    return wrapper
+
+class Decorator(PyAwk):
+    @decorate_action
+    def begin(self):
+        self.print('begin method')
+
+    @decorate_action
+    def action(self, S):
+        self.print(S[1:])
+
+    @decorate_action
+    def end(self):
+        self.print('end method')
+
+if __name__ == '__main__':
+    Decorator().run()
