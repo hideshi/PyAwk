4f891b6d5cd8d7d1b50ffff135b5bd54c484ff0d
Hideshi Ogoshi <hideshi.ogoshi@gmail.com>
2014-02-03 21:00:15 +0900

    Modified in order to use self.print method

--- examples/fixedlength.py
+++ examples/fixedlength.py
@@ -2,10 +2,10 @@
 from pyawk import PyAwk, p
 class FixedLength(PyAwk):
     def begin(self):
-        self.FIELDWIDTHS = [6,2,4]
+        self.FIELDWIDTHS = [6,3,4]
 
     def action(self, S):
-        print(S[0], S[1], S[2], S[3])
+        self.print(S[0], S[1], S[2], S[3])
 
 if __name__ == '__main__':
     FixedLength().run()
